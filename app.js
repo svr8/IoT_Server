0800465139243d44d4c3b6aa56de54463361dbe4
svr8 <shikhar.vaish90@gmail.com>
2019-03-22 13:46:06 +0530

    Updated response time

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const ejs = require('ejs');
 const path = require('path');
 const serialport = require("serialport");
 const port = 8081;
+const serial_addr = '/dev/ttyUSB0';
+//const serial_addr = '/dev/ttyAMA0';
 
 var sensorList = {
     'S1': [],
     'S2': [],
     'S3': [],
     'S4': [],
-    'S5': [],
-    'S6': []
+    'S5': []
 }
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
@@ -27,59 +28,57 @@ app.get('/s1', function(req, res) {
     let arr = sensorList['S1'];
     let value = arr[arr.length-1];
     sensorList['S1'] = [];
-    res.send(value);
+    res.send(`${value}`);
+    /*res.writeHead(200, {"Content-Type":"application/json"});
+    res.write(JSON.stringify({"val": value}));*/
 });
 
 app.get('/s2', function(req, res) {
     let arr = sensorList['S2'];
     let value = arr[arr.length-1];
     sensorList['S2'] = [];
-    res.send(value);
+    res.send(`${value}`);
 });
 
 app.get('/s3', function(req, res) {
     let arr = sensorList['S3'];
     let value = arr[arr.length-1];
     sensorList['S3'] = [];
-    res.send(value);
+    res.send(`${value}`);
 });
 
 app.get('/s4', function(req, res) {
     let arr = sensorList['S4'];
     let value = arr[arr.length-1];
     sensorList['S4'] = [];
-    res.send(value);
+    res.send(`${value}`);
 });
 
 app.get('/s5', function(req, res) {
     let arr = sensorList['S5'];
     let value = arr[arr.length-1];
-    sensorList['S5'] = [];
-    res.send(value);
-});
-
-app.get('/s6', function(req, res) {
-    let arr = sensorList['S6'];
-    let value = arr[arr.length-1];
-    sensorList['S6'] = [];
-    res.send(value);
+    //sensorList['S5'] = [];
+    res.send(`${value}`);
 });
 
 
-// serialport.list(function (err, ports) {
-//     ports.forEach(function(port) {
+/*
+ serialport.list(function (err, ports) {
+     ports.forEach(function(port) {
 
-//         console.log("pnpId: " + port.pnpId);
-//         console.log("manufacturer: " + port.manufacturer);
-//         console.log("comName: " + port.comName);
-//         console.log("serialNumber: " + port.serialNumber);
-//         console.log("vendorId: " + port.vendorId);
-//         console.log("productId: " + port.productId);
-//     });
-// });
+         console.log("pnpId: " + port.pnpId);
+         console.log("manufacturer: " + port.manufacturer);
+         console.log("comName: " + port.comName);
+         console.log("serialNumber: " + port.serialNumber);
+         console.log("vendorId: " + port.vendorId);
+         console.log("productId: " + port.productId);
+     });
+ });*/
 
-var arduinoPort = new serialport("/dev/ttyACM0", { // replace ttyACM0 with the right port on your computer
-    baudRate: 9600,
+
+
+var arduinoPort = new serialport(serial_addr, { // replace ttyACM0 with the right port on your computer
+    baudRate: 115200,
     //parser: serialport.parsers.readline("\r\n")
 });
 
@@ -88,16 +87,16 @@ var buff = "";
 arduinoPort.open(function(){
 	arduinoPort.on('data', function(data){
 		let str = data.toString('utf8');
-//		console.log(str);
+		console.log(str);
 		buff = buff + str;
 		flag++;
-		if(flag == 3) {
+		if(flag == 1) {
 			let start = buff.indexOf("S");
 			let end = buff.indexOf("#");
 			let data = buff.substring(start,end);
 			let sensor_prefix = buff.substring(start, start+2);
 			let sensor_value = buff.substring(start+3, end);
-			sensorList[sensor_prefix].push(sensor_value);	
+			sensorList[sensor_prefix].push(sensor_value);
 			buff = buff.substring(end+1);
 			flag = 0;
 		}
@@ -106,11 +105,18 @@ arduinoPort.open(function(){
 
 
 
+
 var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
    
    console.log(`http://localhost:${port}`);
+   
+   /*setInterval( function() {
+       for(let i=1;i<6;i++) {
+	   sensorList[`S${i}`].push( Math.random()*100 );
+       }
+   }, 250 );*/
 });
 
 function isNumeric(num){
--- static/script.js
+++ static/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-	$("#intro").hide();
 	$("#dashboard-wrap").hide();
+	$("#intro").hide();
+
 
 	const sensor_title = [
-		'Ultrasonic Sensor 1',
-		'Ultrasonic Sensor 2',
+		'Ultrasonic Sensor Left',
+		'Ultrasonic Sensor Right',
 		'Heart Rate',
 		'Temperature: Battery',
 		'Temperature: Motor'
@@ -16,7 +17,7 @@ $(document).ready(function() {
 		'°C',
 		'°C'		
 	];
-	let theta = 5;
+	let theta = 1;
 	
 	let alarm_status = false;
 	const sensorCount = 5;
@@ -44,27 +45,35 @@ $(document).ready(function() {
 	}
 
 	const reset_alarm = function() {
-		$(".sensor:nth-child(6)").removeClass('triggered');
-		$(".sensor:nth-child(6)").addClass('not-triggered');
+		$(".sensor:nth-child(5)").removeClass('triggered');
+		$(".sensor:nth-child(5)").addClass('not-triggered');
 	};
 	const set_alarm = function() {
-		$(".sensor:nth-child(6)").removeClass('not-triggered');
-		$(".sensor:nth-child(6)").addClass('triggered');
+		$(".sensor:nth-child(5)").removeClass('not-triggered');
+		$(".sensor:nth-child(5)").addClass('triggered');
 	}
 	
 
 	setInterval(function(){ 
 		
 	for(let i=0; i<sensorCount;i++) {
-		let sensorID = `S${(i+1)}`;
+		let sensorID = `s${(i+1)}`;
 		let domID = i;
 		getSensorValue(sensorID, function(data){
-			setSensorValue(domID, data);
+			if(data && data!='undefined')
+				setSensorValue(domID, data);
+			//if(i==2) console.log(data);
 		});
 	}
 	
-	let ultrasonic_val = Number((document.getElementsByClassName("sensor-value")[0].innerHTML).trim());
-	let status = ultrasonic_val <= theta;
+	/*
+	getSensorValue('s1', function(data) {
+		console.log(data);
+	});*/
+	
+	let ultrasonic_val_left = Number((document.getElementsByClassName("sensor-value")[0].innerHTML).trim());
+	let ultrasonic_val_right = Number((document.getElementsByClassName("sensor-value")[1].innerHTML).trim());
+	let status = ultrasonic_val_left <= theta || ultrasonic_val_right <= theta;
 	if(status && !alarm_status) {
 		alarm_status = true;
 		set_alarm();
@@ -72,7 +81,7 @@ $(document).ready(function() {
 		alarm_status = false;
 		reset_alarm();
 	}
-	}, 250);
+	}, 100);
 
 	$("#intro").fadeIn(1200, "linear", function(){
 		$("#intro").delay(600).fadeOut(1200, "linear", function(){
